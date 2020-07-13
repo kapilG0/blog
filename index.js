7295edacdd6124ee813edeb9788a24903b408030
kapilG0 <kapilkggupta2@gmail.com>
2020-07-13 11:45:57 +0530

    blog

---
+++ index.js
@@ -0,0 +1,18 @@
+const express=require('express')
+const mongoose =require('mongoose')
+const Article=require('./models/article')
+const articleRouter=require('./routes/article')
+
+const methodOverride=require('method-override')
+const app = express()
+mongoose.connect('mongodb://localhost/blog',{useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true})
+app.set('view engine','ejs')
+app.use(express.urlencoded({extended:false}))
+app.use(methodOverride('_method'))
+app.use('/articles',  articleRouter)
+app.get('/',async(req,res)=>{
+    const articles= await Article.find({}).sort({createdAt:'desc'})
+    res.render('articles/index',{articles:articles})
+})
+
+app.listen(5000,console.log(`server started at ${5000}`))
